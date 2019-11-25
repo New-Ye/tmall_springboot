6caf436274184a0b4ef110ef343e678b821cb1b3
New-Ye <2092100020@qq.com>
2019-11-25 12:56:10 +0800

    新增分页效果（未完成）

---
+++ src/main/java/com/zxc/tmall/util/Page4Navigator.java
@@ -0,0 +1,187 @@
+package com.zxc.tmall.util;
+
+import org.springframework.data.domain.Page;
+import java.util.List;
+
+public class Page4Navigator<T> {
+    Page<T> pageFromJPA;
+    int navigatePages;//导航页码数
+
+    int totalPages;//总页数
+
+    int number;
+
+    long totalElements;
+
+    int size;
+
+    int numberOfElements;
+
+    List<T> content;
+
+    boolean isHasContent;
+
+    boolean first;
+
+    boolean last;
+
+    boolean isHasNext;
+
+    boolean isHasPrevious;
+
+    int[] navigatepageNums;
+
+    public Page4Navigator() {
+        //这个空的分页是为了 Redis 从 json格式转换为 Page4Navigator 对象而专门提供的
+    }
+
+    public Page4Navigator(Page<T> pageFromJPA, int navigatePages) {
+        this.pageFromJPA = pageFromJPA;
+        this.navigatePages = navigatePages;
+
+        totalPages = pageFromJPA.getTotalPages();
+
+        number  = pageFromJPA.getNumber() ;
+
+        totalElements = pageFromJPA.getTotalElements();
+
+        size = pageFromJPA.getSize();
+
+        numberOfElements = pageFromJPA.getNumberOfElements();
+
+        content = pageFromJPA.getContent();
+
+        isHasContent = pageFromJPA.hasContent();
+
+        first = pageFromJPA.isFirst();
+
+        last = pageFromJPA.isLast();
+
+        isHasNext = pageFromJPA.hasNext();
+
+        isHasPrevious  = pageFromJPA.hasPrevious();
+
+        calcNavigatepageNums();
+
+    }
+
+    public void calcNavigatepageNums(){
+        int calcNavigatepageNums[];
+        int totalPages=getTotalPages();
+        int num=getNumber();
+
+
+    }
+    public Page<T> getPageFromJPA() {
+        return pageFromJPA;
+    }
+
+    public void setPageFromJPA(Page<T> pageFromJPA) {
+        this.pageFromJPA = pageFromJPA;
+    }
+
+    public int getNavigatePages() {
+        return navigatePages;
+    }
+
+    public void setNavigatePages(int navigatePages) {
+        this.navigatePages = navigatePages;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public void setNumberOfElements(int numberOfElements) {
+        this.numberOfElements = numberOfElements;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    public boolean isHasContent() {
+        return isHasContent;
+    }
+
+    public void setHasContent(boolean hasContent) {
+        isHasContent = hasContent;
+    }
+
+    public boolean isFirst() {
+        return first;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
+    }
+
+    public boolean isHasNext() {
+        return isHasNext;
+    }
+
+    public void setHasNext(boolean hasNext) {
+        isHasNext = hasNext;
+    }
+
+    public boolean isHasPrevious() {
+        return isHasPrevious;
+    }
+
+    public void setHasPrevious(boolean hasPrevious) {
+        isHasPrevious = hasPrevious;
+    }
+
+    public int[] getNavigatepageNums() {
+        return navigatepageNums;
+    }
+
+    public void setNavigatepageNums(int[] navigatepageNums) {
+        this.navigatepageNums = navigatepageNums;
+    }
+
+}
